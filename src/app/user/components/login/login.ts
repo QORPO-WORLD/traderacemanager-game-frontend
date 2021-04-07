2e143ebd2b4512b8a77f2f14df419de16b7e2762
Adrian Golian <adriangolian@gmail.com>
2021-04-07 21:06:13 +0200

    bugset after merge

--- src/app/user/components/login/login.ts
+++ src/app/user/components/login/login.ts
@@ -83,7 +83,7 @@ export class LoginComponent extends AbstractComponent implements OnInit, OnDestr
         this.submitted = true;
         this.mmewa = mmew;
 
-        if (location.href === 'https://traderacemanager.com/user/sign-in') {
+        if (location.href === 'https://dev-play.traderacemanager.com/user/sign-in') {
           this.submit();
         }
 
