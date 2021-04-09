3ef640b4424ec1f0c2d4afdcf8b64012fb155b9f
TomMas1 <maslanaktomas@gmail.com>
2021-04-09 23:32:03 +0200

    rewards done, repaired texts, outgame shop

--- src/app/others/pages/about-tokens/about-tokens.component.ts
+++ src/app/others/pages/about-tokens/about-tokens.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { RewardsService } from 'src/app/api/services';
-import { BalanceService } from 'src/app/common/services/balance.service';
-import { AuthService } from 'src/app/user/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { RewardsService } from "src/app/api/services";
+import { BalanceService } from "src/app/common/services/balance.service";
+import { AuthService } from "src/app/user/services/auth.service";
+import { NotifyService } from "./../../../common/services/notify.service";
 
 @Component({
-  selector: 'app-about-tokens',
-  templateUrl: './about-tokens.component.html',
-  styleUrls: ['./about-tokens.component.scss'],
+  selector: "app-about-tokens",
+  templateUrl: "./about-tokens.component.html",
+  styleUrls: ["./about-tokens.component.scss"],
 })
 export class AboutTokensComponent implements OnInit {
-
   transObserver: Subscription;
   ioioreward: number;
   myRewards: any;
@@ -18,22 +18,49 @@ export class AboutTokensComponent implements OnInit {
   rewardLevelMax = 0;
   isLevel = 1;
   Affilate: any;
-  constructor(private rwrdsrvc: RewardsService, private identityService: AuthService) { }
+  myAfiilateShort = "";
+  myAfiilate = "https://traderacemanager.com/user/referral/";
+  dumbreferral: string;
+
+  constructor(
+    private rwrdsrvc: RewardsService,
+    private identityService: AuthService,
+    private notify: NotifyService
+  ) {}
 
   ngOnInit() {
+    this.getAffilateLink();
     this.getRewards();
-    const balance = JSON.parse(localStorage.getItem('user-balance'));
+    const balance = JSON.parse(localStorage.getItem("user-balance"));
     this.getrewardLevel(balance.game_wallet_ioi);
     this.getMyLevel();
-
   }
 
+  copyInputMessage() {
+    let selBox = document.createElement("textarea");
+    selBox.style.position = "fixed";
+    selBox.style.left = "0";
+    selBox.style.top = "0";
+    selBox.style.opacity = "0";
+    selBox.value = this.myAfiilate;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand("copy");
+    document.body.removeChild(selBox);
+    this.notify.error("Link copied to clipboard");
+  }
+  getAffilateLink() {
+    const data = this.identityService.getStorageIdentity();
+
+    this.myAfiilateShort = data.affiliate_slug;
+    this.myAfiilate += data.affiliate_slug;
+  }
   getRewards() {
-    this.transObserver = this.rwrdsrvc.rewardsList()
-      .subscribe(data => {
-        const datax: any = data;
-        this.ioioreward = Number(datax.ioi_bonus);
-      });
+    this.transObserver = this.rwrdsrvc.rewardsList().subscribe((data) => {
+      const datax: any = data;
+      this.ioioreward = Number(datax.ioi_bonus);
+    });
   }
   getMyLevel() {
     this.Affilate = this.identityService.getStorageAff();
@@ -43,7 +70,7 @@ export class AboutTokensComponent implements OnInit {
     if (data < 100) {
       this.isLevel = 1;
       this.rewardLevel = data;
-      this.rewardLevelMax = 100 / (100 / data) ;
+      this.rewardLevelMax = 100 / (100 / data);
     }
     if (data > 100 && data < 1000) {
       this.isLevel = 2;
@@ -65,8 +92,5 @@ export class AboutTokensComponent implements OnInit {
       this.rewardLevel = data;
       this.rewardLevelMax = 100;
     }
-
   }
-
-
 }
