6c116f81aece3f949fa2a7599a2481f2a1b92be1
Adrian Golian <adriangolian@gmail.com>
2021-04-15 13:18:38 +0200

    create owner

--- src/app/teams/components/owner/owner.component.ts
+++ src/app/teams/components/owner/owner.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { NotifiqService } from 'src/app/common/services/notifiq.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { TeamsService } from 'src/app/api/services';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-owner',
   templateUrl: './owner.component.html',
   styleUrls: ['./owner.component.scss'],
 })
-export class OwnerComponent implements OnInit {
+export class OwnerComponent implements OnInit, OnDestroy {
+  ownerSubscribe: Subscription;
+  avatarType = 0;
+  color = '000000';
+  teamName: string;
+  loading = false;
+  constructor(private api: TeamsService, private route: Router, private notify: NotifiqService) { }
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {}
+  ngOnDestroy() {
+    if (this.ownerSubscribe) {
+      this.ownerSubscribe.unsubscribe();
+    }
+  }
+
+  createTeam() {
+    if (this.teamName.length > 0) {
+      this.loading = true;
+      this.ownerSubscribe = this.api.createTeam({
+        "team_name": this.teamName,
+        "color": this.color,
+        "avatar": this.avatarType
+      }).subscribe(data => {
+        this.loading = false;
+        this.teamName = null;
+        this.notify.error('team created','Your team ' + this.teamName + ' has been created!');
+        this.route.navigate(['/teams/my-team']);
+      });
+    }
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+  }
 
 }
