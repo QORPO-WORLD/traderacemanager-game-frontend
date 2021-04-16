e38484df0a9a24b348f21b00eb4b4081ef4c4d73
Kornel <kornel.oselsky9@gmail.com>
2021-04-16 15:37:35 +0200

    owner team colors

--- src/app/teams/components/owner/owner.component.ts
+++ src/app/teams/components/owner/owner.component.ts
@@ -13,15 +13,40 @@ import { AuthService } from 'src/app/user/services/auth.service';
 export class OwnerComponent implements OnInit, OnDestroy {
   ownerSubscribe: Subscription;
   notifySubscribe: Subscription;
-  avatarType = 0;
+  teamType = 1;
   color = '000000';
   teamName: string;
   loading = false;
   managersInvited = false;
   racerInvited = false;
+  startTeamIndex = 0;
+  myTeams: Array<any> = [
+    { color: 'FF7474', id: 0 },
+    { color: 'FF003D', id: 1 },
+    { color: 'FF007A', id: 2 },
+    { color: 'FF00A8', id: 3 },
+    { color: 'FA00FF', id: 4 },
+    { color: 'BD00FF', id: 5 },
+    { color: '7E03DE', id: 6 },
+    { color: '3300FF', id: 7 },
+    { color: '0075FF', id: 8 },
+    { color: '00D1FF', id: 9 },
+    { color: '00FFFF', id: 10 },
+    { color: '00FFE0', id: 11 },
+    { color: '00FF75', id: 12 },
+    { color: 'BDFF00', id: 13 },
+    { color: 'FFF500', id: 14 },
+    { color: 'FFD600', id: 15 },
+    { color: 'FFA800', id: 16 },
+    { color: 'FF7A00', id: 17 },
+    { color: 'A34400', id: 18 },
+    { color: '990000', id: 19 },
+  ];
   constructor(private api: TeamsService, private route: Router, private notify: NotifiqService, private identityService: AuthService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.selectColor(0);
+  }
 
   ngOnDestroy() {
     if (this.ownerSubscribe) {
@@ -38,7 +63,7 @@ export class OwnerComponent implements OnInit, OnDestroy {
       this.ownerSubscribe = this.api.createTeam({
         "team_name": this.teamName,
         "color": this.color,
-        "avatar": this.avatarType
+        "avatar": this.teamType
       }).subscribe(data => {
         this.loading = false;
         this.teamName = null;
@@ -66,6 +91,23 @@ export class OwnerComponent implements OnInit, OnDestroy {
       }
       this.notify.error('', 'Members have been notified!');
     });
+  }
+
+  nextTeam(){
+    if (this.startTeamIndex < this.myTeams.length - 4) {
+      this.startTeamIndex++;
+    } else this.startTeamIndex = 0;
+  }
+
+  prevTeam(){
+    if (this.startTeamIndex > 0) {
+      this.startTeamIndex--;
+    } else this.startTeamIndex = this.myTeams.length - 4;
+  }
+
+  selectColor(id: number){
+    this.teamType = id;
+    this.color = this.myTeams[id].color;
   }
 
 }
