24dec8708070da1cca74ca161f8ae19b31b6f71c
TomMas1 <maslanaktomas@gmail.com>
2021-04-22 10:34:35 +0200

    name update in buy detail

--- src/app/car/buy-nft/buy-nft.component.ts
+++ src/app/car/buy-nft/buy-nft.component.ts
@@ -1,368 +1,372 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { AuthService } from './../../user/services/auth.service';
-import { BalanceService } from './../../common/services/balance.service';
-import { NotifyService } from './../../common/services/notify.service';
-import { CarsService } from '../../api/services';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from "@angular/core";
+import { AuthService } from "./../../user/services/auth.service";
+import { BalanceService } from "./../../common/services/balance.service";
+import { NotifyService } from "./../../common/services/notify.service";
+import { CarsService } from "../../api/services";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-buy-nft',
-  templateUrl: './buy-nft.component.html',
-  styleUrls: ['./buy-nft.component.scss'],
+  selector: "app-buy-nft",
+  templateUrl: "./buy-nft.component.html",
+  styleUrls: ["./buy-nft.component.scss"],
 })
 export class BuyNftComponent implements OnInit {
-
   myIndex: string;
 
   racers: Array<object> = [
     {
       id: 1,
-      collection: 'Bronze',
-      name: 'Axle',
-      prize: '1152',
-      image: 'white-mn',
+      collection: "Bronze",
+      name: "Axle",
+      prize: "1152",
+      image: "white-mn",
     },
     {
       id: 2,
-      collection: 'Bronze',
-      name: 'Flash',
-      prize: '1152',
-      image: 'red-korpo',
+      collection: "Bronze",
+      name: "Flash",
+      prize: "1152",
+      image: "red-korpo",
     },
     {
       id: 3,
-      collection: 'Bronze',
-      name: 'Octane',
-      prize: '1152',
-      image: 'blue-mn',
+      collection: "Bronze",
+      name: "Octane",
+      prize: "1152",
+      image: "blue-mn",
     },
     {
       id: 4,
-      collection: 'Bronze',
-      name: 'Punisher',
-      prize: '1152',
-      image: 'black-korpo',
+      collection: "Bronze",
+      name: "Punisher",
+      prize: "1152",
+      image: "black-korpo",
     },
     {
       id: 5,
-      collection: 'Bronze',
-      name: 'Lady Rich',
-      prize: '1152',
-      image: 'lady-rich',
+      collection: "Bronze",
+      name: "Lady Rich",
+      prize: "1152",
+      image: "lady-rich",
     },
     {
       id: 6,
-      collection: 'Bronze',
-      name: 'Rich Jr.',
-      prize: '1152',
-      image: 'bad-boy',
+      collection: "Bronze",
+      name: "Rich Jr.",
+      prize: "1152",
+      image: "bad-boy",
     },
     {
       id: 7,
-      collection: 'Bronze',
-      name: 'Mr. Rich',
-      prize: '1152',
-      image: 'mr-rich',
+      collection: "Bronze",
+      name: "Mr. Rich",
+      prize: "1152",
+      image: "mr-rich",
     },
     {
       id: 8,
-      collection: 'Bronze',
-      name: 'Mrs. Rich',
-      prize: '1152',
-      image: 'mrs-rich',
+      collection: "Bronze",
+      name: "Mrs. Rich",
+      prize: "1152",
+      image: "mrs-rich",
     },
   ];
   cars: Array<object> = [
     //bronze
     {
       id: 9,
-      collection: 'Bronze Collection',
-      name: 'Bronze 1',
-      prize: '600',
-      image: 'car1',
-      extras: { stake: '0.1', roi: '6', buy_id: 1, value: '600' }
+      collection: "Common",
+      name: "RHINO",
+      prize: "600",
+      image: "car1",
+      extras: { stake: "0.1", roi: "6", buy_id: 1, value: "600" },
     },
     {
       id: 10,
-      collection: 'Bronze Collection',
-      name: 'Bronze 2',
-      prize: '600',
-      image: 'car2',
-      extras: { stake: '0.1', roi: '6', buy_id: 2, value: '600' }
+      collection: "Common",
+      name: "PANTHER",
+      prize: "600",
+      image: "car2",
+      extras: { stake: "0.1", roi: "6", buy_id: 2, value: "600" },
     },
     {
       id: 11,
-      collection: 'Bronze Collection',
-      name: 'Bronze 3',
-      prize: '600',
-      image: 'car3',
-      extras: { stake: '0.1', roi: '6', buy_id: 3, value: '600' }
+      collection: "Common",
+      name: "ONYX",
+      prize: "600",
+      image: "car3",
+      extras: { stake: "0.1", roi: "6", buy_id: 3, value: "600" },
     },
     {
       id: 12,
-      collection: 'Bronze Collection',
-      name: 'Bronze 4',
-      prize: '600',
-      image: 'car4',
-      extras: { stake: '0.1', roi: '6', buy_id: 4, value: '600' }
+      collection: "Common",
+      name: "ZANDER",
+      prize: "600",
+      image: "car4",
+      extras: { stake: "0.1", roi: "6", buy_id: 4, value: "600" },
     },
     {
       id: 13,
-      collection: 'Bronze Collection',
-      name: 'Bronze 5',
-      prize: '600',
-      image: 'car5',
-      extras: { stake: '0.1', roi: '6', buy_id: 5, value: '600' }
+      collection: "Common",
+      name: "CYBORG",
+      prize: "600",
+      image: "car5",
+      extras: { stake: "0.1", roi: "6", buy_id: 5, value: "600" },
     },
     {
       id: 14,
-      collection: 'Bronze Collection',
-      name: 'Bronze 6',
-      prize: '600',
-      image: 'car6',
-      extras: { stake: '0.1', roi: '6', buy_id: 6, value: '600' }
+      collection: "Common",
+      name: "VULCANIC",
+      prize: "600",
+      image: "car6",
+      extras: { stake: "0.1", roi: "6", buy_id: 6, value: "600" },
     },
     {
       id: 15,
-      collection: 'Bronze Collection Rare',
-      name: 'Bronze 7',
-      prize: '3 600',
-      image: 'car25',
+      collection: "Common rare",
+      name: "LUNA",
+      prize: "3 600",
+      image: "car25",
       rare: true,
-      extras: { stake: '0.6', roi: '6', buy_id: 25, value: '3600' }
+      extras: { stake: "0.6", roi: "6", buy_id: 25, value: "3600" },
     },
     //silver
     {
       id: 16,
-      collection: 'Silver Collection',
-      name: 'Silver 1',
-      prize: '1 000',
-      image: 'car7',
-      extras: { stake: '0.33', roi: '12', buy_id: 7, value: '1000' }
+      collection: "Super",
+      name: "DORIAN",
+      prize: "1 000",
+      image: "car7",
+      extras: { stake: "0.33", roi: "12", buy_id: 7, value: "1000" },
     },
     {
       id: 17,
-      collection: 'Silver Collection',
-      name: 'Silver 2',
-      prize: '1 000',
-      image: 'car8',
-      extras: { stake: '0.33', roi: '12', buy_id: 8, value: '1000' }
+      collection: "Super",
+      name: "PANTHER",
+      prize: "1 000",
+      image: "car8",
+      extras: { stake: "0.33", roi: "12", buy_id: 8, value: "1000" },
     },
     {
       id: 18,
-      collection: 'Silver Collection',
-      name: 'Silver 3',
-      prize: '1 000',
-      image: 'car9',
-      extras: { stake: '0.33', roi: '12', buy_id: 9, value: '1000' }
+      collection: "Super",
+      name: "ONYX",
+      prize: "1 000",
+      image: "car9",
+      extras: { stake: "0.33", roi: "12", buy_id: 9, value: "1000" },
     },
     {
       id: 19,
-      collection: 'Silver Collection',
-      name: 'Silver 4',
-      prize: '1 000',
-      image: 'car10',
-      extras: { stake: '0.33', roi: '12', buy_id: 10, value: '1000' }
+      collection: "Super",
+      name: "ZANDER",
+      prize: "1 000",
+      image: "car10",
+      extras: { stake: "0.33", roi: "12", buy_id: 10, value: "1000" },
     },
     {
       id: 20,
-      collection: 'Silver Collection',
-      name: 'Silver 5',
-      prize: '1 000',
-      image: 'car11',
-      extras: { stake: '0.33', roi: '12', buy_id: 11, value: '1000' }
+      collection: "Super",
+      name: "PYTHON",
+      prize: "1 000",
+      image: "car11",
+      extras: { stake: "0.33", roi: "12", buy_id: 11, value: "1000" },
     },
     {
       id: 21,
-      collection: 'Silver Collection',
-      name: 'Silver 6',
-      prize: '1 000',
-      image: 'car12',
-      extras: { stake: '0.33', roi: '12', buy_id: 12, value: '1000' }
+      collection: "Super",
+      name: "VULCANIC",
+      prize: "1 000",
+      image: "car12",
+      extras: { stake: "0.33", roi: "12", buy_id: 12, value: "1000" },
     },
     {
       id: 22,
-      collection: 'Silver Collection Rare',
-      name: 'Silver 7',
-      prize: '6 000',
-      image: 'car26',
+      collection: "Super rare",
+      name: "SILVER KNIGHT",
+      prize: "6 000",
+      image: "car26",
       rare: true,
-      extras: { stake: '1.98', roi: '12', buy_id: 26, value: '6000' }
+      extras: { stake: "1.98", roi: "12", buy_id: 26, value: "6000" },
     },
     //gold
     {
       id: 23,
-      collection: 'Gold Collection',
-      name: 'Gold 1',
-      prize: '1 600',
-      image: 'car13',
-      extras: { stake: '0.79', roi: '18', buy_id: 13, value: '1600' }
+      collection: "Epic",
+      name: "CYBORG",
+      prize: "1 600",
+      image: "car13",
+      extras: { stake: "0.79", roi: "18", buy_id: 13, value: "1600" },
     },
     {
       id: 24,
-      collection: 'Gold Collection',
-      name: 'Gold 2',
-      prize: '1 600',
-      image: 'car14',
-      extras: { stake: '0.79', roi: '18', buy_id: 14, value: '1600' }
+      collection: "Epic",
+      name: "RHINO",
+      prize: "1 600",
+      image: "car14",
+      extras: { stake: "0.79", roi: "18", buy_id: 14, value: "1600" },
     },
     {
       id: 25,
-      collection: 'Gold Collection',
-      name: 'Gold 3',
-      prize: '1 600',
-      image: 'car15',
-      extras: { stake: '0.79', roi: '18', buy_id: 15, value: '1600' }
+      collection: "Epic",
+      name: "HYPER",
+      prize: "1 600",
+      image: "car15",
+      extras: { stake: "0.79", roi: "18", buy_id: 15, value: "1600" },
     },
     {
       id: 26,
-      collection: 'Gold Collection',
-      name: 'Gold 4',
-      prize: '1 600',
-      image: 'car16',
-      extras: { stake: '0.79', roi: '18', buy_id: 16, value: '1600' }
+      collection: "Epic",
+      name: "BULL",
+      prize: "1 600",
+      image: "car16",
+      extras: { stake: "0.79", roi: "18", buy_id: 16, value: "1600" },
     },
     {
       id: 27,
-      collection: 'Gold Collection',
-      name: 'Gold 5',
-      prize: '1 600',
-      image: 'car17',
-      extras: { stake: '0.79', roi: '18', buy_id: 17, value: '1600' }
+      collection: "Epic",
+      name: "PYTHON",
+      prize: "1 600",
+      image: "car17",
+      extras: { stake: "0.79", roi: "18", buy_id: 17, value: "1600" },
     },
     {
       id: 28,
-      collection: 'Gold Collection',
-      name: 'Gold 6',
-      prize: '1 600',
-      image: 'car18',
-      extras: { stake: '0.79', roi: '18', buy_id: 18, value: '1600' }
+      collection: "Epic",
+      name: "HITMAN",
+      prize: "1 600",
+      image: "car18",
+      extras: { stake: "0.79", roi: "18", buy_id: 18, value: "1600" },
     },
     {
       id: 29,
-      collection: 'Gold Collection Rare',
-      name: 'Gold 7',
-      prize: '9 600',
-      image: 'car27',
+      collection: "Epic rare",
+      name: "MIDAS",
+      prize: "9 600",
+      image: "car27",
       rare: true,
-      extras: { stake: '4.74', roi: '18', buy_id: 27, value: '9600' }
+      extras: { stake: "4.74", roi: "18", buy_id: 27, value: "9600" },
     },
     //platinum
     {
       id: 30,
-      collection: 'Platinum Collection',
-      name: 'Platinum 1',
-      prize: '2 600',
-      image: 'car19',
-      extras: { stake: '1.71', roi: '24', buy_id: 19, value: '2600' }
+      collection: "Legendary",
+      name: "HYPER",
+      prize: "2 600",
+      image: "car19",
+      extras: { stake: "1.71", roi: "24", buy_id: 19, value: "2600" },
     },
     {
       id: 31,
-      collection: 'Platinum Collection',
-      name: 'Platinum 2',
-      prize: '2 600',
-      image: 'car20',
-      extras: { stake: '1.71', roi: '24', buy_id: 20, value: '2600' }
+      collection: "Legendary",
+      name: "DORIAN",
+      prize: "2 600",
+      image: "car20",
+      extras: { stake: "1.71", roi: "24", buy_id: 20, value: "2600" },
     },
     {
       id: 32,
-      collection: 'Platinum Collection',
-      name: 'Platinum 3',
-      prize: '2 600',
-      image: 'car21',
-      extras: { stake: '1.71', roi: '24', buy_id: 21, value: '2600' }
+      collection: "Legendary",
+      name: "VULCANIC",
+      prize: "2 600",
+      image: "car21",
+      extras: { stake: "1.71", roi: "24", buy_id: 21, value: "2600" },
     },
     {
       id: 33,
-      collection: 'Platinum Collection',
-      name: 'Platinum 4',
-      prize: '2 600',
-      image: 'car22',
-      extras: { stake: '1.71', roi: '24', buy_id: 22, value: '2600' }
+      collection: "Legendary",
+      name: "BULL",
+      prize: "2 600",
+      image: "car22",
+      extras: { stake: "1.71", roi: "24", buy_id: 22, value: "2600" },
     },
     {
       id: 34,
-      collection: 'Platinum Collection',
-      name: 'Platinum 5',
-      prize: '2 600',
-      image: 'car23',
-      extras: { stake: '1.71', roi: '24', buy_id: 23, value: '2600' }
+      collection: "Legendary",
+      name: "KNOCKOUT",
+      prize: "2 600",
+      image: "car23",
+      extras: { stake: "1.71", roi: "24", buy_id: 23, value: "2600" },
     },
     {
       id: 35,
-      collection: 'Platinum Collection',
-      name: 'Platinum 6',
-      prize: '2 600',
-      image: 'car24',
-      extras: { stake: '1.71', roi: '24', buy_id: 24, value: '2600' }
+      collection: "Legendary",
+      name: "LARA",
+      prize: "2 600",
+      image: "car24",
+      extras: { stake: "1.71", roi: "24", buy_id: 24, value: "2600" },
     },
     {
       id: 36,
-      collection: 'Platinum Collection Rare',
-      name: 'Platinum 7',
-      prize: '15 600',
-      image: 'car28',
+      collection: "Legendary rare",
+      name: "BLUE STORM",
+      prize: "15 600",
+      image: "car28",
       rare: true,
-      extras: { stake: '10.25', roi: '24', buy_id: 28, value: '15600' }
+      extras: { stake: "10.25", roi: "24", buy_id: 28, value: "15600" },
     },
   ];
   tracks: Array<object> = [
     {
       id: 37,
-      collection: 'Race tracks',
-      name: 'Racer',
-      prize: '1152',
-      image: 'track',
-      extras: { stake: '0.1', roi: '6' }
+      collection: "Race tracks",
+      name: "Racer",
+      prize: "1152",
+      image: "track",
+      extras: { stake: "0.1", roi: "6" },
     },
     {
       id: 38,
-      collection: 'Race tracks',
-      name: 'Beach Race',
-      prize: '1152',
-      image: 'track',
-      extras: { stake: '0.1', roi: '6' }
+      collection: "Race tracks",
+      name: "Beach Race",
+      prize: "1152",
+      image: "track",
+      extras: { stake: "0.1", roi: "6" },
     },
     {
       id: 39,
-      collection: 'Race tracks',
-      name: 'Forest Race',
-      prize: '1152',
-      image: 'track',
-      extras: { stake: '0.1', roi: '6' }
+      collection: "Race tracks",
+      name: "Forest Race",
+      prize: "1152",
+      image: "track",
+      extras: { stake: "0.1", roi: "6" },
     },
     {
       id: 40,
-      collection: 'Race tracks',
-      name: 'Space Race',
-      prize: '1152',
-      image: 'track',
-      extras: { stake: '0.1', roi: '6' }
+      collection: "Race tracks",
+      name: "Space Race",
+      prize: "1152",
+      image: "track",
+      extras: { stake: "0.1", roi: "6" },
     },
   ];
 
   displayArray = [];
-  @Input() assetType = 'racer';
+  @Input() assetType = "racer";
   @Input() assetId = 1;
   amount = 1;
   myDriverBalances: any;
 
-  constructor(protected api: CarsService, private balanceService: BalanceService, private identityService: AuthService,
-    private notify: NotifyService, private router: Router) { }
+  constructor(
+    protected api: CarsService,
+    private balanceService: BalanceService,
+    private identityService: AuthService,
+    private notify: NotifyService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.resolveBuyAsset();
     this.getMydriverBalances();
   }
 
-  resolveBuyAsset(){
-    if (this.assetType === 'racer') {
+  resolveBuyAsset() {
+    if (this.assetType === "racer") {
       this.displayArray = this.racers;
     }
-    if (this.assetType === 'car') {
+    if (this.assetType === "car") {
       this.displayArray = this.cars;
     }
-    if (this.assetType === 'track') {
+    if (this.assetType === "track") {
       this.displayArray = this.tracks;
     }
     this.displayArray = this.displayArray.filter(
@@ -370,11 +374,11 @@ export class BuyNftComponent implements OnInit {
     );
   }
 
-  plusAmount(){
+  plusAmount() {
     this.amount++;
   }
 
-  minusAmount(){
+  minusAmount() {
     if (this.amount > 0) {
       this.amount--;
     }
@@ -382,15 +386,14 @@ export class BuyNftComponent implements OnInit {
 
   buyCarFromGarage(index: number) {
     this.myIndex = index.toString();
-    this.api.carsBuyList(this.myIndex).
-      subscribe(datax => {
-        const data: any = datax;
-        setTimeout(() => {
-          this.notifyChangedBalance();
-          this.router.navigate(['/car/garage/my-cars']);
-          this.notify.error('You have bought a new car!');
-        }, 1000);
-      });
+    this.api.carsBuyList(this.myIndex).subscribe((datax) => {
+      const data: any = datax;
+      setTimeout(() => {
+        this.notifyChangedBalance();
+        this.router.navigate(["/car/garage/my-cars"]);
+        this.notify.error("You have bought a new car!");
+      }, 1000);
+    });
   }
 
   notifyChangedBalance() {
@@ -401,5 +404,4 @@ export class BuyNftComponent implements OnInit {
   getMydriverBalances() {
     this.myDriverBalances = this.identityService.getBalance();
   }
-
 }
