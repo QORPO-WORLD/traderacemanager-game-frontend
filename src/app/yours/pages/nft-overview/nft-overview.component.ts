1131cb202348bca127775ec8ec193c1ff471c600
TomMas1 <maslanaktomas@gmail.com>
2021-04-30 18:45:15 +0200

    last updates before release

--- src/app/yours/pages/nft-overview/nft-overview.component.ts
+++ src/app/yours/pages/nft-overview/nft-overview.component.ts
@@ -1,5 +1,5 @@
-import { MyNftDetailComponent } from "./../my-nft-detail/my-nft-detail.component";
-import { MyNftComponent } from "../my-nft/my-nft.component";
+import { TeamsService } from "src/app/api/services";
+
 import { Component, OnInit } from "@angular/core";
 import { Chart } from "angular-highcharts";
 import { Options } from "highcharts";
@@ -8,7 +8,7 @@ import { AuthService } from "src/app/user/services/auth.service";
 import { AuthService as ninja } from "./../../../user/services/auth.service";
 import { BalanceService } from "./../../../common/services/balance.service";
 import { bindCallback, Subscription } from "rxjs";
-import { setInterval } from "timers";
+import { LeaderboardService } from "src/app/api/services";
 
 @Component({
   selector: "app-nft-overview",
@@ -18,18 +18,27 @@ import { setInterval } from "timers";
 export class NftOverviewComponent implements OnInit {
   myCarsObserver: Subscription;
   typeObserver: Subscription;
-  display = window.innerWidth;
-  allItems;
-  allCars;
-  racers;
-  cars;
-  tracks;
-  teams;
-  carAssets;
+  teamSubscription: Subscription;
+  display: any = window.innerWidth;
+  allItems: number;
+
+  allCars: any;
+  racers: any;
+  cars: any;
+  tracks: any;
+  teamsCount: number;
+  teams: any;
+  allTeams: any;
+  carAssets: any;
+  myTeamAllData: any;
   totalStaking: number;
-  special;
-  donutChart;
-  active = "car";
+  special: any;
+  donutChart: any;
+  accountInfo: any;
+  active: number = 1;
+  interval: any;
+  myTeam: string;
+  myTeamData: any;
   donutChartOptions: Options = {
     chart: {
       type: "pie",
@@ -45,7 +54,7 @@ export class NftOverviewComponent implements OnInit {
     plotOptions: {
       pie: {
         innerSize: "88%",
-        borderWidth: 7,
+        borderWidth: 4,
         borderColor: "#222222",
 
         dataLabels: {
@@ -71,44 +80,85 @@ export class NftOverviewComponent implements OnInit {
   };
 
   constructor(
+    protected apit: TeamsService,
     protected api: CarsService,
+    private lead: LeaderboardService,
     private auth: AuthService,
-    private iservice: ninja
+    private iservice: ninja,
+    private identityService: AuthService
   ) {
+    this.getMyTeam();
+    this.getMyTem();
     this.pushTo();
+    this.accountInfo = this.iservice.getDriverMe();
+    this.totalStaking = this.accountInfo.total_staking_rewards;
   }
 
   ngOnInit() {
+    this.changeActive(1);
     this.width();
-    const data = this.iservice.getDriverMe();
-    this.totalStaking = data.total_staking_rewards;
   }
 
   getMydriver() {
-    const data = this.auth.getDriverMe();
+    this.accountInfo = this.auth.getDriverMe();
   }
   calcItems() {
-    this.allItems = this.allCars;
+    setTimeout(() => {
+      if (this.teamsCount > 0) {
+        this.allItems =
+          this.carAssets.garage_value + this.myTeamAllData.team_value;
+      } else {
+        this.allItems = this.carAssets.garage_value;
+      }
+    }, 1000);
   }
   width() {
     this.display = window.innerWidth;
   }
-  changeActive() {}
+
+  setInterval() {
+    this.interval = window.setInterval(() => {
+      if (this.teamsCount > 0) {
+        if (this.active <= 4) {
+          this.active++;
+        }
+        if (this.active > 4) {
+          this.active = 1;
+        }
+      }
+      if (this.teamsCount === 0) {
+        if (this.active <= 3) {
+          this.active++;
+        }
+        if (this.active > 3) {
+          this.active = 1;
+        }
+      }
+    }, 5000);
+  }
+  changeActive(p) {
+    this.active = p;
+    this.clearActiveInterval();
+    this.setInterval();
+  }
+  clearActiveInterval() {
+    clearInterval(this.interval);
+  }
   pushTo() {
     this.myCarsObserver = this.api.carsMineList().subscribe((data) => {
       const objs: any = data;
       this.allCars = objs.cars.length;
       this.carAssets = data;
-
-      this.racers = { name: "Racers", y: 23, color: "#00f0ff" };
+      this.isOwner();
+      this.teams = this.racers = { name: "Racers", y: 0, color: "#00f0ff" };
       this.cars = {
         name: "Cars",
         y: this.allCars,
         color: "#fe173f",
       };
-      this.tracks = { name: "Tracks", y: 40, color: "#A46100" };
-      this.teams = { name: "Teams", y: 35, color: "#00ee91" };
-      this.special = { name: "Special", y: 15, color: "gold" };
+      this.tracks = { name: "Tracks", y: 0, color: "#A46100" };
+      this.teams = { name: "Teams", y: this.teamsCount, color: "#00ee91" };
+      this.special = { name: "Special", y: 0, color: "gold" };
       this.donutChartOptions.series[0]["data"].push(this.racers);
       this.donutChartOptions.series[0]["data"].push(this.cars);
       this.donutChartOptions.series[0]["data"].push(this.tracks);
@@ -117,5 +167,38 @@ export class NftOverviewComponent implements OnInit {
       this.donutChart = new Chart(this.donutChartOptions);
       this.calcItems();
     });
+  }
+  isOwner() {
+    if (this.myTeamData.is_owner === true) {
+      this.teamsCount = 1;
+    } else {
+      this.teamsCount = 0;
+    }
+  }
+  getTeams() {
+    this.apit.teamsList().subscribe((data) => {
+      const newdata = data.results;
+      const resort = newdata.sort((a, b) => {
+        return b.dedicated_team_bonus_pool - a.dedicated_team_bonus_pool;
+      });
+
+      this.allTeams = data.results;
+    });
+  }
+
+  getMyTeam() {
+    const data = this.identityService.getDriverMe();
+    this.myTeam = data.team;
+    this.getTeams();
+    this.myTeamData = data;
+  }
+  getMyTem() {
+    this.teamSubscription = this.lead
+      .leaderboardTeamInternalList()
+      .subscribe((datax) => {
+        const data: any = datax;
+        this.myTeamAllData = data;
+        console.log(this.myTeamAllData);
+      });
   }
 }
