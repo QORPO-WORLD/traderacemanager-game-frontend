c59b95ce005a9974e76228166e1c175585fd3e58
Adrian Golian <adriangolian@gmail.com>
2021-05-05 10:14:57 +0200

    websocket integration + signup bug

--- src/app/race/components/binary-race/binary-race.component.ts
+++ src/app/race/components/binary-race/binary-race.component.ts
@@ -1,12 +1,25 @@
+import { timeStamp } from 'console';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
 import { Chart as lineChart } from 'angular-highcharts';
 import { AuthService } from 'src/app/user/services/auth.service';
-
+import { map, catchError, distinctUntilChanged, pairwise, tap } from 'rxjs/operators';
+import { Observable, Subject, EMPTY, of, interval } from 'rxjs';
 declare let ccxt: any;
-declare let ccxws: any;
 import Highcharts, { Axis } from 'highcharts';
 import { min } from 'rxjs/operators';
+
+import { webSocket, WebSocketSubject } from "rxjs/webSocket";
+export interface Trade {
+  data: {
+    p: number,
+    s: string,
+    t: number,
+    v: number
+  }[],
+  type: string
+}
+
 @Component({
   selector: 'app-binary-race',
   templateUrl: './binary-race.component.html',
@@ -27,21 +40,32 @@ export class BinaryRaceComponent implements OnInit {
   chartMin = 54000;
   menuOpen = false;
   myDriverStats: any;
-  raceEnded = false; 
+  raceEnded = false;
 
   @ViewChild("unityRace", { static: false }) raceComp: any;
-
+  myChart: any;
+  myCoin: any;
+  socket$: WebSocketSubject<any> = webSocket({
+    url: 'wss://ws.finnhub.io?token=bsr37a748v6tucpfplbg',
+    openObserver: {
+      next: () => {
+        this.socket$.next({ 'type': 'subscribe', 'symbol': 'BINANCE:BTCUSDT' });
+      }
+    },
+  });
+  price$: Observable<any>;
+  direction$: Observable<any> = of('green');
   constructor(private identityService: AuthService) { }
 
   ngOnInit() {
     setTimeout(() => {
-      let chart = Highcharts.chart('container',{
+      this.myChart = Highcharts.chart('container', {
         chart: {
-            type: 'areaspline',
-            scrollablePlotArea: {
-              minWidth: 100,
-              scrollPositionX: 1
-            }
+          type: 'areaspline',
+          scrollablePlotArea: {
+            minWidth: 100,
+            scrollPositionX: 1
+          }
         },
         yAxis: {
           offset: 50,
@@ -53,37 +77,34 @@ export class BinaryRaceComponent implements OnInit {
           min: this.chartMin
         },
         series: [{
-            name: 'Price',
-            type: 'areaspline',
-            data: [...this.chartData]
+          name: 'Price',
+          type: 'areaspline',
+          data: [...this.chartData]
         }]
       });
-      
-      let counter = 0;
-      
-      this.chartInterval = setInterval(() => {
-        this.add(chart);
-        this.updatePlotLine(chart);
-        counter++;
-        if (counter === 10) {
-          this.makeItStop();
-        }
-      }, this.timeStamp);
-    }, 100);
-      
-    this.getMyDriver();
 
+      let counter = 0;
+
+
+    }, 100);
+
+    //this.runScheduler();
+    this.getMyDriver();
+    this.price$ = this.getLatestPrice();
   }
 
-  add(chart: any) {
-    this.currentValue = Math.floor(Math.random() * (60000 - 54000 + 1)) + 54000;
+  add() {
+    //this.currentValue = Math.floor(Math.random() * (60000 - 54000 + 1)) + 54000;
+
+    console.log(Math.floor(Math.random() * (60000 - 54000 + 1)) + 54000);
+    console.log(this.currentValue);
     this.timeStamp++;
     this.chartData.push(this.currentValue);
-    chart.series[0].addPoint([(this.timeStamp / 1000), this.currentValue]);
-    this.adjustChartAxis(chart);
+    this.myChart.series[0].addPoint([(this.timeStamp / 1000), this.currentValue]);
+    this.adjustChartAxis();
   }
 
-  updatePlotLine(chart: any){
+  updatePlotLine(chart: any) {
     this.plotYvalue = this.currentValue;
     chart.yAxis[0].removePlotBand('line');
     chart.yAxis[0].addPlotLine({
@@ -91,49 +112,49 @@ export class BinaryRaceComponent implements OnInit {
       dashStyle: 'Solid',
       value: this.plotYvalue,
       width: 2,
-      label: {align: 'right', text: '' + this.plotYvalue, x: -10, useHTML: true},
+      label: { align: 'right', text: '' + this.plotYvalue, x: -10, useHTML: true },
       zIndex: 10,
       id: 'line'
     })
   }
 
-  adjustChartAxis(chart: any){
+  adjustChartAxis() {
 
     if (this.chartData.length > 20) {
       this.chartData.shift();
-      chart.series[0].data[0].remove(true, true);
+      this.myChart.series[0].data[0].remove(true, true);
     }
 
     if (this.currentValue > this.chartMax) {
       this.chartMax = this.currentValue;
-      chart.yAxis[0].update({
+      this.myChart.yAxis[0].update({
         max: this.chartMax
-    });
+      });
     }
 
     if (this.currentValue < this.chartMin) {
       this.chartMax = this.currentValue;
-      chart.yAxis[0].update({
+      this.myChart.yAxis[0].update({
         min: this.chartMin
-    });
+      });
     }
   }
 
-  getMyDriver(){
+  getMyDriver() {
     this.myDriverStats = this.identityService.getDriverMe();
   }
 
-  makeItStop(){
+  makeItStop() {
     clearInterval(this.chartInterval);
   }
 
-  hello(){
+  hello() {
     this.raceComp.hello();
   }
-  yo(){
+  yo() {
     this.raceComp.yo();
   }
-  good(){
+  good() {
     this.raceComp.good();
   }
 
@@ -148,7 +169,7 @@ export class BinaryRaceComponent implements OnInit {
     const p = this.btcmeno['average'] * Math.random();
     this.mainChart.push(p);
 
-    
+
     //console.log(test);
 
     //console.log(kraken);
@@ -163,7 +184,7 @@ export class BinaryRaceComponent implements OnInit {
     //let okcoinusd = new ccxt.okcoinusd();
 
     //let okcoinusd = new ccxt.okcoinusd();
-    
+
     /*
         const exchangeId = 'binance'
           , exchangeClass = ccxt[exchangeId]
@@ -195,7 +216,7 @@ export class BinaryRaceComponent implements OnInit {
     // use a custom order type
     //bitfinex.createLimitSellOrder('BTC/USD', 1, 10, { 'type': 'trailing-stop' });
 
-  
+
   }
 
   fireBtc() {
@@ -261,11 +282,70 @@ export class BinaryRaceComponent implements OnInit {
   }
 
   createChart() {
-      
+
   }
 
   updateChart() {
     this.lineChart.series[0].setData(...this.mainChart);
     this.lineChart.redraw();
+  }
+
+  async runScheduler() {
+    /*
+     const exchange = new ccxt.binance({ enableRateLimit: true });
+  if (exchange.has['watchTicker']) {
+
+    try {
+      const ticker = await exchange.watchTicker('BTC/USDT');
+      this.myCoin = {
+        time: ticker.timeStamp,
+        value: ticker.last
+      };
+    } catch (e) {
+      console.log(e)
+      // stop the loop on exception or leave it commented to retry
+      // throw e
+    }
+
+  }
+
+ 
+     this.chartInterval = setInterval(() => {
+    this.add();
+    this.updatePlotLine(this.myChart);
+  }, 5000);
+  */
+
+
+  }
+
+
+  getLatestPrice() {
+    return this.socket$.pipe(
+      map((t: Trade) => t.type === 'trade' && t.data[0].p.toFixed()),
+      distinctUntilChanged(),
+      tap(d => this.setPriceToGraph(d)),
+      catchError(_ => EMPTY)
+    )
+  }
+
+  setPriceToGraph(data) {
+    const time = Date.now();
+    //this.timeStamp = time;
+    this.currentValue = data;
+
+    //console.log(time);
+    //console.log(data);
+    this.add();
+  }
+
+  getDirection() {
+    return this.getLatestPrice().pipe(
+      pairwise(),
+      // tap(d => {
+      //   console.log(`Current val ${d[1]} > ${d[0]} `, d[1] > d[0])
+      // }),
+      map(arr => arr[0] < arr[1] ? 'green' : 'red')
+    )
   }
 }
