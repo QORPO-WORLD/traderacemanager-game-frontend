fe8b7c1a6d6922c8bfc15ee8df2380b75f6e96e4
Adam Prisenznak <adam.prisen@gmail.com>
2021-06-08 08:08:49 +0200

    team leaderboard without landscape

--- src/app/teams/components/my-team-players/my-team-players.component.ts
+++ src/app/teams/components/my-team-players/my-team-players.component.ts
@@ -14,12 +14,14 @@ export class MyTeamPlayersComponent implements OnInit, OnDestroy {
   @Input() mr: any;
   @Input() payouts: any;
   @Input() mrp: any;
-  myLdrbrd: any;
+  //myLdrbrd: any;
   mydrvr: any;
   display = window.innerWidth;
   myStatData: any;
+  myTeamName: any;
   myLdrbrdObserver: Subscription;
   drSubscription: Subscription;
+  actualPage = 1;
   teamId: number;
   slice1: number;
   slice2: number;
@@ -34,6 +36,68 @@ export class MyTeamPlayersComponent implements OnInit, OnDestroy {
     3: 3,
     4: 3,
   };
+  myLdrbrd: Array<object> = [
+    {
+      position: 1,
+      team_name:  "BTC",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 2,
+      team_name:  "IOI",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 3,
+      team_name:  "ALT",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 4,
+      team_name:  "TRX",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 5,
+      team_name:  "TRX",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 6,
+      team_name:  "TRX",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 7,
+      team_name:  "TRX",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 8,
+      team_name:  "TRX",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 9,
+      team_name:  "TRX",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    },
+    {
+      position: 10,
+      team_name:  "TRX",
+      team_turnover: 10000,
+      team_potentional_win: 100
+    }
+  ];
   constructor(
     protected ldrbrdSrvc: LeaderboardService,
     private drvrsrvc: DriversService,
@@ -44,6 +108,7 @@ export class MyTeamPlayersComponent implements OnInit, OnDestroy {
     // this.getMyDriver();
     //this.getMyLeaderboard();
     this.getMyTeam();
+    this.getMyLeaderboard();
     this.width();
   }
 
@@ -57,14 +122,15 @@ export class MyTeamPlayersComponent implements OnInit, OnDestroy {
   }
 
   getMyLeaderboard() {
-    this.myLdrbrdObserver = this.ldrbrdSrvc
-      .leaderboardMe({
-        page: 1,
-        lastMonth: false,
-      })
-      .subscribe((data) => {
-        this.myLdrbrd = data;
-      });
+    // this.myLdrbrdObserver = this.ldrbrdSrvc
+    //   .leaderboardTeamList({
+    //     page: 1,
+    //     lastMonth: false
+    //   })
+    //   .subscribe((data) => {
+    //     this.myLdrbrd = data;
+    //   });
+
   }
 
   width() {
@@ -78,6 +144,7 @@ export class MyTeamPlayersComponent implements OnInit, OnDestroy {
   getMyTeam() {
     const data = this.identityService.getLeaderboardMe();
     this.teamId = data.team_id;
+    this.myTeamName = data.team_name;
     this.getteamStats();
   }
 
@@ -86,4 +153,7 @@ export class MyTeamPlayersComponent implements OnInit, OnDestroy {
       this.myStatData = data;
     });
   }
+
+  
+
 }
