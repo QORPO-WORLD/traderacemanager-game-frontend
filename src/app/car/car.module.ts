ec1e105f8d30c46f00b8a3ff81ef922d00ae03ec
TomMas1 <maslanaktomas@gmail.com>
2021-06-08 22:53:32 +0200

    working on refuel......

--- src/app/car/car.module.ts
+++ src/app/car/car.module.ts
@@ -1,53 +1,60 @@
-import { EditFastFuelCarComponent } from './../common/components/shared/edit-fast/edit-fast.component';
-import { FavouriteCarsComponent } from './favourite-cars/favourite-cars.component';
+import { EditFastFuelCarComponent } from "./../common/components/shared/edit-fast/edit-fast.component";
+import { FavouriteCarsComponent } from "./favourite-cars/favourite-cars.component";
 
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
 
-import { Routes, RouterModule } from '@angular/router';
-import { BuyCarsComponent } from './buy-cars/buy-cars.component';
-import { FuelCarComponent } from './fuel-car/fuel-car.component';
-import { RefuelCarComponent } from './refuel-car/refuel-car.component';
-import { CommonModule as ninja } from '../common/common.module';
-import { CommonModule } from '@angular/common';
-import { RaceLayoutComponent } from '../common/components/layout/race-layout/race-layout.component';
-import { SiteLayoutComponent } from '../common/components/layout/default/site-layout/site-layout.component';
-import { AuthUserGuard } from '../user/services/guards/auth-user.guard';
-import { OrderModule } from 'ngx-order-pipe';
-import { IonicModule } from '@ionic/angular';
-import { MyCarsComponent } from './my-cars/my-cars.component';
-import { DragScrollModule } from 'ngx-drag-scroll';
-import { NftMarketComponent } from './nft-market/nft-market.component';
-import { NftDetailComponent } from './nft-detail/nft-detail.component';
-import { BuyNftComponent } from './buy-nft/buy-nft.component';
-import { FuelLayoutComponent } from '../common/components/layout/fuel-layout/fuel-layout.component';
+import { Routes, RouterModule } from "@angular/router";
+import { BuyCarsComponent } from "./buy-cars/buy-cars.component";
+import { FuelCarComponent } from "./fuel-car/fuel-car.component";
+import { RefuelCarComponent } from "./refuel-car/refuel-car.component";
+import { CommonModule as ninja } from "../common/common.module";
+import { CommonModule } from "@angular/common";
+import { RaceLayoutComponent } from "../common/components/layout/race-layout/race-layout.component";
+import { SiteLayoutComponent } from "../common/components/layout/default/site-layout/site-layout.component";
+import { AuthUserGuard } from "../user/services/guards/auth-user.guard";
+import { OrderModule } from "ngx-order-pipe";
+import { IonicModule } from "@ionic/angular";
+import { MyCarsComponent } from "./my-cars/my-cars.component";
+import { DragScrollModule } from "ngx-drag-scroll";
+import { NftMarketComponent } from "./nft-market/nft-market.component";
+import { NftDetailComponent } from "./nft-detail/nft-detail.component";
+import { BuyNftComponent } from "./buy-nft/buy-nft.component";
+import { FuelLayoutComponent } from "../common/components/layout/fuel-layout/fuel-layout.component";
 
 const routes: Routes = [
   {
-    path: '',
-    component: FuelLayoutComponent,
-    children: [
-      { path: '', redirectTo: 'garage/my-cars', pathMatch: 'prefix' },
-      { path: 'fuel-car/:id', component: FuelCarComponent }
-    ]
-  },
-  {
-    path: '',
+    path: "",
     component: SiteLayoutComponent,
     children: [
-      { path: '', redirectTo: 'garage/my-cars', pathMatch: 'prefix' },
-      { path: 'garage', component: BuyCarsComponent },
-      { path: 'garage/my-cars', component: MyCarsComponent },
-      { path: 'favourite-cars', component: FavouriteCarsComponent },
-      { path: 'nft-market', component: NftMarketComponent }
-    ]
-  }
+      { path: "", redirectTo: "garage/my-cars", pathMatch: "prefix" },
+      { path: "fuel-car/:id", component: FuelCarComponent },
+    ],
+  },
+  {
+    path: "",
+    component: SiteLayoutComponent,
+    children: [
+      { path: "", redirectTo: "garage/my-cars", pathMatch: "prefix" },
+      { path: "garage", component: BuyCarsComponent },
+      { path: "garage/my-cars", component: MyCarsComponent },
+      { path: "favourite-cars", component: FavouriteCarsComponent },
+      { path: "nft-market", component: NftMarketComponent },
+    ],
+  },
 ];
 @NgModule({
-  declarations: [BuyCarsComponent, FuelCarComponent,
-    MyCarsComponent, RefuelCarComponent,
-    FavouriteCarsComponent, EditFastFuelCarComponent, NftMarketComponent, NftDetailComponent,
-    BuyNftComponent],
+  declarations: [
+    BuyCarsComponent,
+    FuelCarComponent,
+    MyCarsComponent,
+    RefuelCarComponent,
+    FavouriteCarsComponent,
+    EditFastFuelCarComponent,
+    NftMarketComponent,
+    NftDetailComponent,
+    BuyNftComponent,
+  ],
   imports: [
     CommonModule,
     ninja,
@@ -55,8 +62,8 @@ const routes: Routes = [
     FormsModule,
     OrderModule,
     IonicModule,
-    DragScrollModule
+    DragScrollModule,
   ],
-  exports: [RefuelCarComponent]
+  exports: [RefuelCarComponent],
 })
-export class CarModule { }
+export class CarModule {}
