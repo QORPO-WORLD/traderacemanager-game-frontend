385d15f502d4c6cc19803f960715e15c36aacebb
TomMas1 <maslanaktomas@gmail.com>
2021-06-28 12:58:58 +0200

    disable google auth

--- src/app/user/pages/disgauthr/disgauthr.component.ts
+++ src/app/user/pages/disgauthr/disgauthr.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MfaService } from 'src/app/api/services/mfa.service';
-import { Subscription } from 'rxjs';
-import { DriversService } from 'src/app/api/services';
-import { Router } from '@angular/router';
-import { NotifiqService } from 'src/app/common/services/notifiq.service';
-import { TranslateService } from '@ngx-translate/core';
-import { AuthService } from 'src/app/user/services/auth.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { MfaService } from "src/app/api/services/mfa.service";
+import { Subscription } from "rxjs";
+import { DriversService } from "src/app/api/services";
+import { Router } from "@angular/router";
+import { NotifiqService } from "src/app/common/services/notifiq.service";
+import { TranslateService } from "@ngx-translate/core";
+import { AuthService } from "src/app/user/services/auth.service";
 
 @Component({
-  selector: 'app-gauthr',
-  templateUrl: './disgauthr.component.html',
-  styleUrls: ['./disgauthr.component.scss'],
+  selector: "app-gauthr",
+  templateUrl: "./disgauthr.component.html",
+  styleUrls: ["./disgauthr.component.scss"],
 })
 export class DisGauthrComponent implements OnInit, OnDestroy {
+  isManager: boolean;
   curStep = 0;
   xObserver: Subscription;
   dObserver: Subscription;
@@ -22,9 +23,14 @@ export class DisGauthrComponent implements OnInit, OnDestroy {
   qrkey: any;
   testinp: string;
   pass: string;
-  constructor(private mfsrvc: MfaService, private drvrsrvc: DriversService,
-              private route: Router, private notify: NotifiqService, protected translate: TranslateService,
-              private identityService: AuthService) { }
+  constructor(
+    private mfsrvc: MfaService,
+    private drvrsrvc: DriversService,
+    private route: Router,
+    private notify: NotifiqService,
+    protected translate: TranslateService,
+    private identityService: AuthService
+  ) {}
 
   ngOnInit() {
     //this.getKey();
@@ -32,7 +38,6 @@ export class DisGauthrComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-
     if (this.xObserver) {
       this.xObserver.unsubscribe();
     }
@@ -48,45 +53,56 @@ export class DisGauthrComponent implements OnInit, OnDestroy {
   }
 
   getKey() {
-    this.xObserver = this.mfsrvc.mfaSecretCreate().subscribe(data => {
+    this.xObserver = this.mfsrvc.mfaSecretCreate().subscribe((data) => {
       const oldata: any = data;
       this.myKey = oldata.secret;
     });
   }
   getQrKey() {
-    this.dObserver = this.mfsrvc.mfaQrSecretCreate().subscribe(data => {
+    this.dObserver = this.mfsrvc.mfaQrSecretCreate().subscribe((data) => {
       this.createImageFromBlob(data);
     });
   }
 
   testKey() {
-    this.tObserver = this.mfsrvc.mfaCancelCreate({ code: this.testinp }).subscribe(data => {
-      if (data) {
-        if (data.result === true) {
-          this.identityService.logout();
-        } else {
-          this.translate.get('nitro_notifiq').subscribe((res) => {
-            this.notify.error('x', res.wrong_code);
-          });
+    this.tObserver = this.mfsrvc
+      .mfaCancelCreate({ code: this.testinp })
+      .subscribe((data) => {
+        if (data) {
+          if (data.result === true) {
+            this.identityService.logout();
+          } else {
+            this.translate.get("nitro_notifiq").subscribe((res) => {
+              this.notify.error("x", res.wrong_code);
+            });
+          }
         }
-      }
-    });
+      });
   }
 
   cancelKey() {
-    this.route.navigate(['/race/start-race']);
+    this.route.navigate(["/race/start-race"]);
   }
-  
 
   createImageFromBlob(image: Blob) {
     const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      this.qrkey = reader.result;
-    }, false);
+    reader.addEventListener(
+      "load",
+      () => {
+        this.qrkey = reader.result;
+      },
+      false
+    );
     if (image) {
       reader.readAsDataURL(image);
     }
   }
-
-
+  recognizeManager() {
+    const man = this.identityService.getDriverMe().mode;
+    if (man === "owner") {
+      this.isManager = true;
+    } else {
+      this.isManager = false;
+    }
+  }
 }
