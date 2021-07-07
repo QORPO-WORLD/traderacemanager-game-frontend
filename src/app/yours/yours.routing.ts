7ef748c1eb815e5fd037c8e3438ad43ad7eb04ea
Adam Prisenznak <adam.prisen@gmail.com>
2021-07-07 17:23:44 +0200

    remove rewards

--- src/app/yours/yours.routing.ts
+++ src/app/yours/yours.routing.ts
@@ -2,7 +2,7 @@ import { MyProfileComponent } from "./pages/my-profile/my-profile.component";
 import { TransactionsComponent } from "./../yours/pages/transactions/transactions.component";
 import { SiteLayoutComponent } from "../common/components/layout/default/site-layout/site-layout.component";
 import { RouterModule, Routes } from "@angular/router";
-import { RewardsComponent } from "../others/pages/rewards/rewards.component";
+//import { RewardsComponent } from "../others/pages/rewards/rewards.component";
 import { AffilateComponent } from "../others/pages/affilate/affilate.component";
 import { ModuleWithProviders } from "@angular/core";
 import { AuthUserGuard } from "../user/services/guards/auth-user.guard";
