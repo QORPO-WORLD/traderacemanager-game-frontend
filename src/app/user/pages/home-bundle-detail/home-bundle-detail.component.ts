87555edc540c1710ebf4e99debb4005aa1d30c8c
TomMas1 <maslanaktomas@gmail.com>
2021-07-20 16:50:53 +0200

    new bundle detail and market detail update

--- src/app/user/pages/home-bundle-detail/home-bundle-detail.component.ts
+++ src/app/user/pages/home-bundle-detail/home-bundle-detail.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
-import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-home-bundle-detail",
@@ -8,86 +6,232 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ["./home-bundle-detail.component.scss"],
 })
 export class HomeBundleDetailComponent implements OnInit {
-  bundleBack: string;
-  backTo: string;
-  bundles: Array<object> = [
+  products: Array<object> = [
     {
       id: 1,
+      position: 0,
+      type: "bundle",
       name: "DAOMaker",
-      carId: 41,
-      avatarName: "dao-maker",
+      link: "@TheDaoMaker",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
       image: "bundle-dao",
+      racer: "dao-maker",
+      car: "car41",
+      avatar: "dao-avatar",
+      back: "dao-back",
     },
     {
       id: 2,
-      name: "Shreyansh",
-      carId: 42,
-      avatarName: "polygon",
+      position: 1,
+      type: "bundle",
+      name: "Shreyansh Polygon",
+      link: "@shreyansh_27",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
       image: "bundle-polygon",
+      racer: "polygon",
+      car: "car42",
     },
     {
       id: 3,
+      position: 3,
+      type: "bundle",
       name: "Kyle Chasse",
-      carId: 44,
-      avatarName: "paid",
+      link: "@kyle_chasse",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
       image: "bundle-kyle",
+      racer: "paid",
+      car: "car44",
+      avatar: "paid-avatar",
+      back: "paid-back",
     },
     {
       id: 4,
+      position: 4,
+      type: "bundle",
       name: "Ash WSB",
-      carId: 45,
-      avatarName: "ash-wsb",
+      link: "@ashWSBreal",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
       image: "bundle-ash",
+      racer: "ash-wsb",
+      car: "car45",
+      avatar: "ash-avatar",
+      back: "ash-back",
     },
     {
       id: 5,
+      position: 5,
+      type: "bundle",
       name: "Tehmoonwalker",
-      carId: 46,
-      avatarName: "tehmoonwalker",
+      link: "@tehMoonwalkeR",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
       image: "bundle-tehmoon",
-    },
-    {
-      id: 6,
-      name: "Parabolic Guy",
-      carId: 47,
-      avatarName: "parabolic-guy",
-      image: "bundle-parabolic",
+      racer: "tehmoonwalker",
+      car: "car46",
+      avatar: "tehmoon-avatar",
+      back: "tehmoon-back",
     },
 
     {
+      id: 6,
+      position: 6,
+      type: "bundle",
+      name: "Parabolic Guy",
+      link: "@GoingParabolic",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
+      image: "bundle-parabolic",
+      racer: "parabolic-guy",
+      car: "car47",
+      avatar: "parabolic-avatar",
+      back: "parabolic-back",
+    },
+    {
       id: 7,
+      position: 9,
+      type: "bundle",
       name: "Altcoin Buzz",
-      carId: 50,
-      avatarName: "altcoin-buzz",
+      link: "@Altcoinbuzzio",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
       image: "bundle-altcoin",
+      racer: "altcoin-buzz",
+      car: "car50",
+      avatar: "altcoin-avatar",
+      back: "altcoin-back",
     },
     {
       id: 8,
+      position: 10,
+      type: "bundle",
       name: "Cryptowizard",
-      carId: 51,
-      avatarName: "cryptowizard",
+      link: "@CryptoWizardd",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
       image: "bundle-wizard",
+      racer: "cryptowizard",
+      car: "car51",
+      avatar: "cryptowizard-avatar",
+      back: "cryptowizard-back",
+    },
+    {
+      id: 9,
+      position: 11,
+      type: "bundle",
+      name: "Kucoin",
+      link: "@kucoincom",
+      nft: 10,
+      staking: 6,
+      reward: 0.1,
+      price: 1000,
+      pieces: 100,
+      yearly: 8760,
+      image: "bundle-kucoin",
+      racer: "kucoin",
+      car: "car52",
+      avatar: "cryptowizard-avatar",
+      back: "cryptowizard-back",
     },
   ];
+  infoRoi = false;
+  infoRoiYearly = false;
+  position: number;
 
-  bundleId = 1;
-  typeObserver: Subscription;
+  displayArray = [];
+  @Input() assetType = "bundle";
+  @Input() assetId = 1;
+  @Input() assetPosition = 1;
+  @Output() modalActive = new EventEmitter<number>();
+  @Output() marketState = new EventEmitter<number>();
 
-  constructor(private route: ActivatedRoute) {}
+  noGifActive = true;
+  gifName = "";
+  animationActive = false;
+  timer = null;
+
+  timer2 = null;
+
+  constructor() {}
 
   ngOnInit() {
-    this.getAssetType();
+    this.resolveShowAsset();
   }
 
-  getAssetType() {
-    this.typeObserver = this.route.queryParams.subscribe((params) => {
-      this.bundleId = +params["id"];
-      this.bundleBack = params["back"];
-      this.backTo = this.bundleBack;
+  showAnimation() {
+    this.animationActive = false;
+    this.noGifActive = true;
+    this.gifName = "none1";
+    this.timer2 = setTimeout(() => {
+      this.gifName = "black-trm-animation";
+      this.animationActive = true;
+      this.noGifActive = false;
+    }, 1);
+    this.timer = setTimeout(() => {
+      this.animationActive = false;
+      this.gifName = "none1";
+      this.noGifActive = true;
+    }, 6500);
+  }
 
-      this.bundles = this.bundles.filter(
-        (item) => item["id"] === this.bundleId
-      );
-    });
+  activateAnimation() {
+    clearTimeout(this.timer2);
+    clearTimeout(this.timer);
+    this.showAnimation();
+  }
+
+  resolveShowAsset() {
+    this.displayArray = this.products.filter(
+      (asset) => asset["id"] === this.assetId
+    );
+    this.position = this.displayArray[0]["position"];
+  }
+
+  showBuyModal() {
+    this.marketState.emit(3);
+  }
+
+  showModal(p) {
+    this.modalActive.emit(p);
+  }
+  back() {
+    {
+      this.marketState.emit(1);
+    }
   }
 }
