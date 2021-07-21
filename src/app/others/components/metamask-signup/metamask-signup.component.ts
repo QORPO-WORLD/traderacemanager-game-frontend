8e29fe8fc3c9b126fb95756e7e08a802669577e8
TomMas1 <maslanaktomas@gmail.com>
2021-07-21 12:50:45 +0200

    login by metamask complete redesign

--- src/app/others/components/metamask-signup/metamask-signup.component.ts
+++ src/app/others/components/metamask-signup/metamask-signup.component.ts
@@ -1,11 +1,9 @@
-import { NotifyService } from './../../../common/services/notify.service';
+import { NotifyService } from "./../../../common/services/notify.service";
 interface ConnectInfo {
   chainId: string;
 }
 
-
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { MetamaskProvider } from "@0xcert/ethereum-metamask-provider";
 import detectEthereumProvider from "@metamask/detect-provider";
 
@@ -13,27 +11,25 @@ const ethprovider = detectEthereumProvider();
 const provider = new MetamaskProvider();
 declare const ethereum: any;
 @Component({
-  selector: 'app-metamask-signup',
-  templateUrl: './metamask-signup.component.html',
-  styleUrls: ['./metamask-signup.component.scss'],
+  selector: "app-metamask-signup",
+  templateUrl: "./metamask-signup.component.html",
+  styleUrls: ["./metamask-signup.component.scss"],
 })
 export class MetamaskSignupComponent implements OnInit {
-
   username: string;
   email: string;
   agreeChecked = false;
   overlay = true;
   chainId = 137;
-  constructor(private notify: NotifyService) { }
+  @Output() modalActive = new EventEmitter<any>();
+  constructor(private notify: NotifyService) {}
 
   ngOnInit() {
     if (window.ethereum) {
-      this.enableMetamask()
-      this.detectEth()
+      this.enableMetamask();
+      this.detectEth();
     }
   }
-
-
 
   async enableMetamask() {
     if (!(await provider.isEnabled())) {
@@ -43,7 +39,7 @@ export class MetamaskSignupComponent implements OnInit {
   }
 
   getBalance() {
-    ethprovider.catch()
+    ethprovider.catch();
   }
 
   async detectEth() {
@@ -62,38 +58,35 @@ export class MetamaskSignupComponent implements OnInit {
   }
 
   setChainId(id: number) {
-    localStorage.setItem('chaind', JSON.stringify(id));
+    localStorage.setItem("chaind", JSON.stringify(id));
     this.chainId = id;
     this.setAddress();
   }
 
   getWalletAddress() {
-
     ethereum
-      .request({ method: 'net_version' })
+      .request({ method: "net_version" })
       .then((data) => this.setChainId(data))
       .catch((error) => {
         if (error.code === 4001) {
-          this.notify.error('Please connect to MetaMask.');
+          this.notify.error("Please connect to MetaMask.");
         } else {
           this.notify.error(error);
         }
       });
-
-
   }
 
   setAddress() {
     ethereum
-    .request({ method: 'eth_requestAccounts' })
-    .then((data) => this.signMessage(data))
-    .catch((error) => {
-      if (error.code === 4001) {
-        this.notify.error('Please connect to MetaMask.');
-      } else {
-        this.notify.error(error);
-      }
-    });
+      .request({ method: "eth_requestAccounts" })
+      .then((data) => this.signMessage(data))
+      .catch((error) => {
+        if (error.code === 4001) {
+          this.notify.error("Please connect to MetaMask.");
+        } else {
+          this.notify.error(error);
+        }
+      });
   }
 
   signMessage(data) {
@@ -101,28 +94,25 @@ export class MetamaskSignupComponent implements OnInit {
     const msgParams = {
       types: {
         EIP712Domain: [
-          { name: 'name', type: 'string' },
-          { name: 'version', type: 'string' },
-          { name: 'chainId', type: 'uint256' },
-          { name: 'verifyingContract', type: 'address' },
-          { name: 'salt', type: 'string' }
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+          { name: "chainId", type: "uint256" },
+          { name: "verifyingContract", type: "address" },
+          { name: "salt", type: "string" },
         ],
-        Msg: [
-          { name: 'text', type: 'string' },
-        ]
+        Msg: [{ name: "text", type: "string" }],
       },
       domain: {
-        name: 'TRM',
-        version: '2.0',
+        name: "TRM",
+        version: "2.0",
         chainId: this.chainId,
-        verifyingContract: '0x8B3870Df408fF4D7C3A26DF852D41034eDa11d81',
-        salt: 'randomstringgg'
+        verifyingContract: "0x8B3870Df408fF4D7C3A26DF852D41034eDa11d81",
+        salt: "randomstringgg",
       },
-      primaryType: 'Msg',
+      primaryType: "Msg",
       message: {
-        text: 'Wallet authentication',
-      }
-
+        text: "Wallet authentication",
+      },
     };
 
     /*
@@ -162,64 +152,65 @@ export class MetamaskSignupComponent implements OnInit {
         let dataTyped = JSON.stringify(eip712TypedData)
     */
     const domain = [
-      { name: 'name', type: 'string' },
-      { name: 'version', type: 'string' },
-      { name: 'chainId', type: 'uint256' },
-      { name: 'verifyingContract', type: 'address' },
-      { name: 'salt', type: 'string' }
+      { name: "name", type: "string" },
+      { name: "version", type: "string" },
+      { name: "chainId", type: "uint256" },
+      { name: "verifyingContract", type: "address" },
+      { name: "salt", type: "string" },
     ];
 
     const domainData = {
-      name: 'TRM',
-      version: '2.0',
+      name: "TRM",
+      version: "2.0",
       chainId: this.chainId,
-      verifyingContract: '0x8B3870Df408fF4D7C3A26DF852D41034eDa11d81',
-      salt: 'randomstringgg'
+      verifyingContract: "0x8B3870Df408fF4D7C3A26DF852D41034eDa11d81",
+      salt: "randomstringgg",
     };
     var message = {
-      text: 'Wallet authentication',
+      text: "Wallet authentication",
     };
 
     const datas = JSON.stringify({
       types: {
         EIP712Domain: domain,
-        Msg: [
-          { name: 'text', type: 'string' },
-        ]
+        Msg: [{ name: "text", type: "string" }],
       },
       domain: domainData,
       primaryType: "Msg",
-      message: message
+      message: message,
     });
 
-    ethereum
-      .sendAsync({
-        method: 'eth_signTypedData_v3',
+    ethereum.sendAsync(
+      {
+        method: "eth_signTypedData_v3",
         params: [signer, datas],
-        from: signer
+        from: signer,
       },
-        (err, result) => {
-          result ? this.finalizeLogin(result) : this.notify.error(err);
-        }
-      );
+      (err, result) => {
+        result ? this.finalizeLogin(result) : this.notify.error(err);
+      }
+    );
   }
 
   finalizeLogin(data) {
     if (data.result) {
       const magic = JSON.stringify(data.result);
-      localStorage.setItem('mmea', magic);
-      localStorage.setItem('mmealq', magic);
+      localStorage.setItem("mmea", magic);
+      localStorage.setItem("mmealq", magic);
       //this.notify.error(magic + ' address associated, finish sign up process now.');
-      setTimeout(() => { this.overlay = false; }, 1300);
       setTimeout(() => {
-        localStorage.removeItem('mmealq');
-       }, 1000000);
+        this.overlay = false;
+      }, 1300);
+      setTimeout(() => {
+        localStorage.removeItem("mmealq");
+      }, 1000000);
     }
   }
 
   refresh() {
     window.location.reload();
   }
-
-
+  closeModal() {
+    this.modalActive.emit();
+  }
 }
