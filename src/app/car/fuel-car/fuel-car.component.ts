bd12fcee5996ca7363f7a0bce19a453d65317230
Kornel Oselsky <kornel.oselsky9@gmail.com>
2021-08-05 16:47:12 +0200

    ticker loss modal close fix, nft tournament car selection limit

--- src/app/car/fuel-car/fuel-car.component.ts
+++ src/app/car/fuel-car/fuel-car.component.ts
@@ -1508,8 +1508,7 @@ export class FuelCarComponent implements OnInit, OnDestroy {
     );
 
     if (!isSituated) {
-
-      if (this.raceIdentifier === 'tesla_tournament_0') {
+      if (this.raceIdentifier === 'tesla_tournament_0' || this.raceIdentifier === 'premium_tournament_0') {
         if (this.validateTeslaCarSelect(index) === 0) {
           this.notify.error("Error", "You can fuel only one car from each edition + one extra tesla car.");
           return;
@@ -2160,7 +2159,7 @@ export class FuelCarComponent implements OnInit, OnDestroy {
     }
 
     if (this.editionIndex === 1) {
-      if ((edition1.length > 0 && this.myCars1[index].extras.tier !== 60) || (edition1.length > 0 && teslas.length > 0)) {
+      if ((edition1.length > 0 && this.myCars1[index].extras.tier !== 60) || (edition1.length > 0 && teslas.length > 0) || (this.myCars1[index].extras.tier === 60 && teslas.length > 0)) {
         return 0;
       } else {
         return 1;
